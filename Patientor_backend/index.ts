a9b6a440d855f6221a582c94ee508e7e8d9ebd1c
tvaskisalo <61991314+tvaskisalo@users.noreply.github.com>
2022-05-02 14:56:02 +0300

    Disabling linting for index.ts for now

--- Patientor_backend/index.ts
+++ Patientor_backend/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable  @typescript-eslint/no-explicit-any */
+/* eslint-disable   */
 const express = require('express');
 const cors = require('cors');
 import diagnosesRouter from './routers/diagnosesRouter';
