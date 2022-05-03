208de67d452bf183c04b850b0b15b53a738981fd
tvaskisalo <61991314+tvaskisalo@users.noreply.github.com>
2022-05-03 11:12:01 +0300

    testing fixes

--- Patientor_backend/index.ts
+++ Patientor_backend/index.ts
@@ -6,7 +6,7 @@ import patientsRouter from './routers/patientsRouter';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'));
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,6 +19,7 @@ app.use('/api/diagnosis',diagnosesRouter);
 
 app.use('/api/patients', patientsRouter);
 
+app.use('/', express.static('build'));
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
